668a47412e0d200f3e5e7f80102a08b0aa45608d
Fatih <fatihayhan9205@gmail.com>
2025-05-23 11:59:35 +0300

    Add files via upload

---
+++ for.cs
@@ -0,0 +1,44 @@
+﻿for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine("Kendime inanıyorum, yazılım işini halledeceğim. ");
+}
+Console.WriteLine("-----------------------------------------------------------------------------------------");
+
+for (int i = 2 ; i < 20; i++)
+{
+    Console.WriteLine($"i değeri {i}");
+}
+Console.WriteLine("-----------------------------------------------------------------------------------------");
+for (int i = 2; i < 20; i+=2)
+{
+    Console.WriteLine($"i değeri {i}");
+}
+
+Console.WriteLine("-----------------------------------------------------------------------------------------");
+
+int sayı1 = 50;
+int sayı2 = 150;
+int toplam = 0;
+for (int i = 51; i < 150; i++)
+{
+    toplam += i;
+}
+Console.WriteLine("50 ile 150 arasındaki sayıların toplamı ="+ toplam);
+Console.WriteLine("-----------------------------------------------------------------------------------------");
+
+int tek = 0;
+int çift = 0;
+for (int i = 0; i < 120; i++)
+{
+    if (i % 2 == 0)
+    {
+        çift++;
+    }
+    else
+    {
+        tek++;
+    }
+}
+Console.WriteLine("0 ile 120 arasındaki çift sayıların sayısı = " + çift);
+Console.WriteLine("0 ile 120 arasındaki tek sayıların sayısı = " + tek);
+
