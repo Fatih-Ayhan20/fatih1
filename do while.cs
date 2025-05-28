364780933bb59626bc1a4134f7094bcf2eb5ee8a
Fatih <fatihayhan9205@gmail.com>
2025-05-29 02:53:35 +0300

    Add files via upload

---
+++ do while.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Limit değerini giriniz: ");
+        int limit = Convert.ToInt32(Console.ReadLine());
+
+        int sayac = 0;
+
+        // WHILE DÖNGÜSÜ
+        Console.WriteLine("\n--- WHILE DÖNGÜSÜ ---");
+        sayac = 0;
+        while (sayac <= limit)
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;
+        }
+
+        // DO-WHILE DÖNGÜSÜ
+        Console.WriteLine("\n--- DO-WHILE DÖNGÜSÜ ---");
+        sayac = 0;
+        do
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;
+        } while (sayac <= limit);
+    }
+}
