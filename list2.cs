841b3f200d8b305a48ad57c6e1a8533ca07783b0
Fatih <fatihayhan9205@gmail.com>
2025-08-29 21:55:29 +0300

    Add files via upload

---
+++ list2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveler = new List<string>();
+
+        Console.WriteLine("☕ 5 kahve ismi giriniz:");
+
+        // 5 kez kullanıcıdan giriş al
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"{i}. kahve: ");
+            string kahveAdi = Console.ReadLine();
+            kahveler.Add(kahveAdi);
+        }
+
+        Console.WriteLine("\n--- Girdiğiniz Kahveler ---");
+
+        // foreach ile listeyi yazdır
+        foreach (var kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
