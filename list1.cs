527b2461837a68ca89a3d022411cc6973db226bb
Fatih <fatihayhan9205@gmail.com>
2025-08-29 19:19:33 +0300

    Add files via upload

---
+++ list1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Davetlilerin isimlerini tutacak liste
+        List<string> davetliler = new List<string>();
+
+        // Listeye isim ekleyelim
+        davetliler.Add("Ahmet");
+        davetliler.Add("Ayşe");
+        davetliler.Add("Mehmet");
+        davetliler.Add("Zeynep");
+        davetliler.Add("Fatma");
+
+        Console.WriteLine(" Patika Plus Gala Gecesi Davetlileri \n");
+
+        // foreach ile listeyi dönelim
+        foreach (var isim in davetliler)
+        {
+            Console.WriteLine(isim);
+        }
+
+        // Alternatif: for döngüsüyle de yazdırabiliriz
+        /*
+        for (int i = 0; i < davetliler.Count; i++)
+        {
+            Console.WriteLine(davetliler[i]);
+        }
+        */
+    }
+}
